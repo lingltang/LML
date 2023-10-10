d5947b395724904d04a9e3035c50da9e32b00bf4
Ling <a1424262219@163.com>
2023-10-10 22:03:55 +0800

    添加测试文件

--- tx/02_nlp/main.py
+++ tx/02_nlp/main.py
@@ -1,18 +1,23 @@
-import torch
-import torch.nn as nn
-import numpy as np
-import math
+# import torch
+# import torch.nn as nn
+# import numpy as np
+# import math
+#
+# from torch.nn import Module
+#
+#
+# class model(nn.Module):
+#     def __init__(self):
+#         super(Module, self).__init__()
+#     def forword(self,x):
+#         print(x)
+#
+# if __name__ == '__main__':
+#     data = torch.tensor([1000,1,3])
+#     m = model()
+#     m(data)
 
-from torch.nn import Module
+def func(**argc):
+    return [argc]
 
-
-class model(nn.Module):
-    def __init__(self):
-        super(Module, self).__init__()
-    def forword(self,x):
-        print(x)
-
-if __name__ == '__main__':
-    data = torch.tensor([1000,1,3])
-    m = model()
-    m(data)
+print(func(1,2,3))
